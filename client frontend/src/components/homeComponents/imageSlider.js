ae94c8fe8b80e8473563262dcf27a57de497bd19
Dat0309 <anhtheboy011@gmail.com>
2022-04-27 10:11:37 +0700

    Update slider

---
+++ client frontend/src/components/homeComponents/imageSlider.js
@@ -0,0 +1,71 @@
+import { useEffect, useState } from "react"
+import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa";
+import { useDispatch, useSelector } from "react-redux";
+import { listProduct } from "../../Redux/Actions/ProductActions";
+import Message from "../LoadingError/Error";
+import Loading from "../LoadingError/Loading";
+
+const ImageSlider = () => {
+
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products, } = productList;
+
+  const [current, setCurrent] = useState(0);
+  const length = products.length;
+
+  useEffect(() => {
+    dispatch(listProduct());
+  }, [dispatch]);
+
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  };
+
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
+
+  if (!Array.isArray(products) || products.length <= 0) {
+    return null;
+  }
+
+  return (
+    <>
+      <section className='slider'>
+        <FaArrowAltCircleLeft className='left-arrow' onClick={prevSlide} />
+        <FaArrowAltCircleRight className='right-arrow' onClick={nextSlide} />
+        {loading ? (
+          <div className="mb-5">
+            <Loading />
+          </div>
+        ) : error ? (
+          <Message variant="alert-danger">{error}</Message>
+        ) : (
+          <>
+            {products.map((slide, index) => {
+              if (slide.rating >= 4) {
+                return (
+                  <div
+                    className={index === current ? 'slide active' : 'slide'}
+                    key={index}
+                  >
+                    {index === current && (
+                      <img src={slide.image} alt='travel image' className='image' />
+                    )}
+                  </div>
+                );
+              }
+              return <></>;
+            })}
+          </>
+
+        )
+        }
+      </section>
+    </>
+
+  );
+};
+
+export default ImageSlider;
--- client frontend/src/screens/HomeScreen.js
+++ client frontend/src/screens/HomeScreen.js
@@ -5,6 +5,7 @@ import ContactInfo from "./../components/homeComponents/ContactInfo";
 import CalltoActionSection from "./../components/homeComponents/CalltoActionSection";
 import Footer from "./../components/Footer";
 import Navbar from "../components/Navbar/NavBar";
+import ImageSlider from "../components/homeComponents/imageSlider";
 
 const HomeScreen = ({ match }) => {
   window.scrollTo(0, 0);
@@ -14,6 +15,7 @@ const HomeScreen = ({ match }) => {
     <div>
       <Header />
       <Navbar/>
+      <ImageSlider />
       <ShopSection keyword={keyword} pagenumber={pagenumber} />
       <CalltoActionSection />
       <ContactInfo />
